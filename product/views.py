34f7efa350eb6e8a46f2bd409f0ca45d0e15c305
jungyoonho128 <wjddbsgh14@gmail.com>
2025-05-21 13:41:13 +0900

    코멘트 3주차 과제

--- product/views.py
+++ product/views.py
@@ -1,7 +1,14 @@
+from gc import get_objects
+
 from .models import MainContent
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 
 def index(request):
     content_list = MainContent.objects.order_by('-pub_date')
     context = {'content_list': content_list}
     return render(request, 'product/content_list.html', context)
+def detail(request, content_id):
+    content_list = get_object_or_404(MainContent, pk=content_id)
+    context = {'content_list': content_list}
+    return render(request, 'product/content_detail.html', context)
